58e8d5bc735c04337a98ac928144953f996ff2bf
Kapil Kumar <18394739+kpilkk@users.noreply.github.com>
2021-09-11 16:36:41 +0530

    rendering and multithreading

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,5 +1,17 @@
+use std::time::Duration;
+use crossterm::terminal::LeaveAlternateScreen;
+use crossterm::cursor::Show;
 use std::error::Error;
 use rusty_audio::Audio;
+use std::io;
+use crossterm::{terminal, ExecutableCommand};
+use crossterm::terminal::EnterAlternateScreen;
+use crossterm::cursor::Hide;
+use crossterm::event::{self, Event, KeyCode};
+use invaders::render;
+use invaders::frame;
+use std::thread;
+use std::sync::mpsc;
 
 fn main() -> Result <(), Box<dyn Error>>{
     let mut audio = Audio::new();
@@ -12,7 +24,56 @@ fn main() -> Result <(), Box<dyn Error>>{
 
     audio.play("startup");
 
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let cur_frame = match render_rx.recv(){
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &cur_frame, false);
+            last_frame = cur_frame;
+        }
+    });
+    // Game loop
+    'gameloop: loop {
+        // Per-frame init
+        let cur_frame = frame::new_frame();
+
+        // Input
+        while event::poll(Duration::default())?{
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code{
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Draw and render
+        let _ = render_tx.send(cur_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
     //CLeanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
